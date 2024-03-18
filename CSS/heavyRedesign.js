2175f20f2ce48cd8e50ed75a6a63789c52c4f16f
Spatulox <M78stormtrooper@laposte.net>
2024-03-18 15:36:20 +0100

    fix "mes services" tabs
    Pages :
    - CV
    - Stages
    - Events
    - Contact

--- CSS/heavyRedesign.js
+++ CSS/heavyRedesign.js
@@ -131,7 +131,6 @@ function getHeavyRedesignCss(){
     }
 
 
-
     .mg_userinfo > a{
       display: flex;
       flex-wrap: nowrap;
@@ -143,7 +142,7 @@ function getHeavyRedesignCss(){
     //   
     // }*/
 
-    #j_idt159{
+    #j_idt159, #j_idt144{
       width: 100%;
     }
 
@@ -184,19 +183,24 @@ function getHeavyRedesignCss(){
       width: auto;
     }*/
 
-    #admissionDoc, #scolarityDoc, #candidateDocListWidget, #stageDocumentsWidget, #litigationInvoiceWidget, #coursesWidget, #coursesSyllabusForm, #studentOpenGestionForm, #projectListForm, #offersForm, #listStudentStageOfferWidget, #openActionListWidget > .mg_content, #openActionListWidget > .mg_title , #marksWidget > div, #missingsWidget > div{
+    #admissionDoc, #scolarityDoc, #candidateDocListWidget, #stageDocumentsWidget, #litigationInvoiceWidget, #coursesWidget, #coursesSyllabusForm, #studentOpenGestionForm, #projectListForm, #offersForm, #listStudentStageOfferWidget, #openActionListWidget > .mg_content, #openActionListWidget > .mg_title , #marksWidget > div, #missingsWidget > div, #eventListWidget > div, #eventPassedListWidget > div, #expWidget >  div, #skillsWidget > div, #trainingsWidget > div, #contactsForm > div, #j_idt175 > div, #eventDetailForm > div, #eventDetailForm > button{
       width: 90%;
       margin: auto;
       /*margin-top: 10px;*/
     }
 
-    #stageDocumentsWidget, #litigationInvoiceWidget, #coursesWidget{
+    #stageDocumentsWidget, #litigationInvoiceWidget, #coursesWidget, #contactsForm > div, #eventDetailForm > div{
       float: inherit;
     }
 
-    #calendar, #annualDocWidget, #admissionDocListWidget, #scolarityDocListWidget, #coursesFilesForm, #syllabusWidget, #studentOpenGestionWidget, #listProjectWidget, #j_idt176, #offersWidget, #openActionListWidget, #marksWidget, #missingsWidget{
+    #calendar, #annualDocWidget, #admissionDocListWidget, #scolarityDocListWidget, #coursesFilesForm, #syllabusWidget, #studentOpenGestionWidget, #listProjectWidget, #j_idt176, #offersWidget, #openActionListWidget, #marksWidget, #missingsWidget, #eventListWidget, #eventPassedListWidget, #expWidget, #skillsWidget, #trainingsWidget, #contactsForm, #j_idt175, #eventDetailForm{
       width: 100%;
       margin: inherit;
+    }
+
+    #eventDetailForm > button{
+      display: block;
+      margin-top: 10px;
     }
 
     /* Avoiding overflow */
