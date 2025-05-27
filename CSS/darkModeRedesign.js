ce39d517b004c2c7050a5ec610fd9e7c1939a463
Spatulox <M78stormtrooper@laposte.net>
2025-05-27 19:39:39 +0200

    Improves dark mode styling consistency.
    Enhances the dark mode experience by ensuring consistent background and text colors across various elements, including widgets and titles.
    
    This change addresses inconsistencies in the previous styling, providing a more unified and visually appealing dark mode.

--- CSS/darkModeRedesign.js
+++ CSS/darkModeRedesign.js
@@ -3,21 +3,26 @@ function getDarkModeCss() {
     :root{
       color-scheme: light dark;
     }
-    *{
+    
+    #mg_portal_body, #mg_portal_content, #mg_portal_center, [class*="ui-"], [class*="ui-"] *, .mg_inherit_bg, .mg_container_group{
       background-color: Canvas !important;
       color: CanvasText !important;
     }
-    .blue.mg_widget_full .mg_title{
+
+    .blue.mg_widget_full .mg_title, .mg_title, [role="row"]> th, .mg_home_title{
       background-color: AccentColor !important;
       color: AccentColorText !important;
     }
-    .blue.mg_widget_full .mg_title *{
+
+    .blue.mg_widget_full .mg_title *,
+    .mg_widget_light .mg_title *,
+    .ui-menuitem-text,
+    .ui-icon,
+    .ui-column-title,
+    .ui-menuitem-link,
+    [role="menuitem"]{
       background-color: unset !important;
       color: unset !important;
-    }
-
-    .mg_widget_light .mg_title{
-      color: AccentColor !important;
     }
 `
     return darkModeStyles
