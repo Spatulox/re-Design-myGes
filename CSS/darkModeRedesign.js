10bcb5b49fd4c4eba02221256ab5ec3e6cdd9a26
Spatulox <M78stormtrooper@laposte.net>
2025-05-27 20:23:43 +0200

    Enhances dark mode appearance and consistency
    Improves dark mode styling by utilizing platform-specific colors (Canvas and AccentColor) in Gecko-based browsers for better integration with the operating system's dark mode settings.
    Also adds general styling improvements for dark mode and non-dark mode styling consistencies.

--- CSS/darkModeRedesign.js
+++ CSS/darkModeRedesign.js
@@ -1,29 +1,36 @@
 function getDarkModeCss() {
+  let gecko = (typeof browser !== 'undefined')
+
   const darkModeStyles = `
-    :root{
-      color-scheme: light dark;
-    }
-    
-    #mg_portal_body, #mg_portal_content, #mg_portal_center, [class*="ui-"], [class*="ui-"] *, .mg_inherit_bg, .mg_container_group{
-      background-color: Canvas !important;
-      color: CanvasText !important;
-    }
+      :root{
+        color-scheme: light dark;
+      }
 
-    .blue.mg_widget_full .mg_title, .mg_title, [role="row"]> th, .mg_home_title{
-      background-color: AccentColor !important;
-      color: AccentColorText !important;
-    }
+      #mg_portal_body,
+      #mg_portal_content,
+      #mg_portal_center,
+      [class*="ui-"]:not(.ui-widget-header):not(.ui-menuitem-link):not(.ui-menuitem-text):not(.ui-icon),
+      [class*="ui-"]:not(.ui-widget-header):not(.ui-menuitem-link):not(.ui-menuitem-text):not(.ui-icon) *,
+      .mg_inherit_bg,
+      .mg_container_group{
+        background-color: ${gecko ? "Canvas" : "#181a1b"} !important;
+        color: ${gecko ? "CanvasText" : "#f5f6fa"} !important;
+      }
+      .blue.mg_widget_full .mg_title, .mg_title, [role="row"]> th, .mg_home_title{
+        background-color: ${gecko ? "AccentColor" : "#2563eb"} !important;
+        color:  ${gecko ? "AccentColorText" : "#ffffff"} !important;
+      }
 
-    .blue.mg_widget_full .mg_title *,
-    .mg_widget_light .mg_title *,
-    .ui-menuitem-text,
-    .ui-icon,
-    .ui-column-title,
-    .ui-menuitem-link,
-    [role="menuitem"]{
-      background-color: unset !important;
-      color: unset !important;
-    }
-`
+      .blue.mg_widget_full .mg_title *,
+      .mg_widget_light .mg_title *,
+      .ui-menuitem-text,
+      .ui-icon,
+      .ui-column-title,
+      .ui-menuitem-link,
+      [role="menuitem"]{
+        background-color: unset !important;
+        color: unset !important;
+      }
+  `
     return darkModeStyles
 }
